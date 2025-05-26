570cc1f7590b4b5e84c9ba7644b15f2eae8e996a
CaeraDenoir11 <monsterbaxter@gmail.com>
2025-05-26 22:30:01 +0800

    repsonsive

--- Rent-A-Girlfriend/src/App.jsx
+++ Rent-A-Girlfriend/src/App.jsx
@@ -4,7 +4,7 @@ import Login from "../frontend/src/components/auth/Login";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="w-full min-h-screen bg-gradient-to-br from-indigo-100 to-white">
       <Login />
     </div>
   );
